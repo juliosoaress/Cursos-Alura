0bdeb5943ea7fde4afea33c3e2ce22e365336331
juliosoaress <79456914+juliosoaress@users.noreply.github.com>
2021-03-01 14:21:57 -0300

    Primeiros programas

---
+++ TestaPontoFlutuante.java
@@ -0,0 +1,33 @@
+
+public class TestaPontoFlutuante {
+	
+	public static void main(String[]args) {
+		
+		double salario = 1250.70;
+		
+		System.out.println("Meu salário é de " +salario+ " reais!");
+		
+		
+		
+		double divisao = 3.14 / 2;
+		
+		System.out.println(divisao);
+		
+		
+		int divisao1 = 5 / 2;
+		
+		System.out.println(divisao1);
+		
+		
+		double divisao2 = 5 / 2;
+		
+		System.out.println(divisao2);
+		
+		
+		double divisao3 = 5.0 / 2;
+		
+		System.out.println(divisao3);
+		
+	}
+
+}
---
+++ TesteVariaveis.java
@@ -0,0 +1,14 @@
+
+public class TesteVariaveis {
+	
+	public static void main (String []args) {
+		
+		System.out.println("ola novo teste");
+		
+		int idade = 32;
+		
+		System.out.println("a idade é " +idade+ " anos!");
+		
+	}
+
+}
